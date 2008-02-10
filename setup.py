f7faed5139b040e6a8d7c87c83b2b2529f5d8366
jaraco <devnull@localhost>
2008-02-10 16:44:16 +0000

    added new setup files

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!python
+
+# $Id$
+
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+setup(name = "svg-chart",
+	version = "1.0.1",
+	description = "Python SVG Charting Library",
+	author = "Jason R. Coombs",
+	author_email = "jaraco@jaraco.com",
+	url = "http://py-svg.sourceforge.net",
+	packages = find_packages('lib'),
+	package_dir = {'':'lib'},
+	install_requires=[
+		# nothing right now
+	],
+	license = "MIT",
+	long_description = """\
+SVG Charting library based on the Ruby SVG::Graph
+
+""",
+	classifiers = [
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"Programming Language :: Python",
+	],
+	entry_points = {
+		#'console_scripts': [
+		#	'cfs_db = cfs.database:interact',
+		#	'cfs_import = cfs.import:do_import',
+		#	'cfs_initialize = cfs.initialize_meta:do_initialize',
+		#	'issue_count_by_topic = cfs.reports:issue_count_by_topic',
+		#	'cfs_add_simulated_data = cfs.simulate:add_data',
+		#	],
+	},
+	)
+
