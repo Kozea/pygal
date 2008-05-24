86bb5266f6f401428d7cc1db09c5070690ce2c17
jaraco <devnull@localhost>
2008-05-24 18:20:35 +0000

    bumped to version 1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ use_setuptools()
 from setuptools import setup, find_packages
 
 setup(name = "svg-chart",
-	version = "1.1",
+	version = "1.2",
 	description = "Python SVG Charting Library",
 	author = "Jason R. Coombs",
 	author_email = "jaraco@jaraco.com",
