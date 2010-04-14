6f87a54be0c8e0691177ecabab9a871f94146004
jaraco <devnull@localhost>
2010-04-14 16:37:22 +0000

    Bumped to 2.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup, find_packages
 
 setup(name = "svg.charts",
-	version = "2.0.3",
+	version = "2.0.4",
 	description = "Python SVG Charting Library",
 	long_description = open('README.txt').read().strip(),
 	author = "Jason R. Coombs",
