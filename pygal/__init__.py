13672619dc7de337ba4685fa6d0df98afab703d2
Florian Mounier <florian.mounier@kozea.fr>
2012-02-01 18:51:11 +0100

    Clean

--- pygal/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-pygal package.
-"""
-
-__all__ = ('graph', 'plot', 'time_series', 'bar', 'pie', 'schedule', 'util')
--- test/tests.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from moulinrouge import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True, threaded=True, host='0.0.0.0', port=21112)
