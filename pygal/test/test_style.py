48f7112ea6c923b6e902ed4e38393dae24a99111
Blake VandeMerwe <blakev@null.net>
2014-09-01 23:55:44 -0600

    Added stroke_width, stroke_style and stroke_dasharray to style.py -- defaults are such that the original style remains the same, but now one can further customize the look of the lines in their graphs. I also added the appropriate unit tests

--- pygal/test/test_style.py
+++ pygal/test/test_style.py
@@ -16,6 +16,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with pygal. If not, see <http://www.gnu.org/licenses/>.
+from pygal.style import Style
 from pygal import Line
 from pygal.style import (
     LightStyle,
@@ -42,3 +43,35 @@ def test_parametric_styles_with_parameters():
     line.add('_', [1, 2, 3])
     line.x_labels = 'abc'
     assert line.render()
+
+def test_stroke_style():
+    s = Style(stroke_style = 'round')
+    assert s.stroke_style == 'round'
+    s = Style(stroke_style = 'bevel')
+    assert s.stroke_style == 'bevel'
+    s = Style(stroke_style = 'miter')
+    assert s.stroke_style == 'miter'
+    s = Style(stroke_style = 'rounded')
+    assert s.stroke_style == 'round'
+    s = Style(stroke_style = 'invalid derp')
+    assert s.stroke_style == 'round'
+
+def test_stroke_dasharray():
+    s = Style(stroke_dasharray = (0,0))
+    assert s.stroke_dasharray == '0,0'
+    s = Style(stroke_dasharray = (.5,.5))
+    assert s.stroke_dasharray == '0,0'
+    s = Style(stroke_dasharray = (.9,.9))
+    assert s.stroke_dasharray == '0,0'
+    s = Style(stroke_dasharray = (1.9,1.9))
+    assert s.stroke_dasharray == '1,1'
+
+def test_stroke_dasharray_input_types():
+    s = Style(stroke_dasharray = (0,0))
+    assert s.stroke_dasharray == '0,0'
+    s = Style(stroke_dasharray = '0,0')
+    assert s.stroke_dasharray == '0,0'
+    s = Style(stroke_dasharray = '0x0')
+    assert s.stroke_dasharray == '0,0'
+    s = Style(stroke_dasharray = '0  0')
+    assert s.stroke_dasharray == '0,0'
