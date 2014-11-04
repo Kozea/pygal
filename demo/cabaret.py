7d13be3052fdab42ccb9ada1d204585103e42636
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2014-11-04 11:31:43 +0100

    Don't launch cabaret when it's not __main__

--- demo/cabaret.py
+++ demo/cabaret.py
@@ -58,4 +58,5 @@ else:
     add_w_builtin()
     app.wsgi_app = WdbMiddleware(app.wsgi_app, start_disabled=True)
 
-app.run(debug=True, threaded=True, host='0.0.0.0', port=12221)
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True, host='0.0.0.0', port=12221)
