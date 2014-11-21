70449fb4f96c1743756ee4062941d4d45f385154
Florian Mounier <florian.mounier@kozea.fr>
2014-11-21 10:22:28 +0100

    Fix format error on list like in table

--- pygal/util.py
+++ pygal/util.py
@@ -38,6 +38,8 @@ def float_format(number):
 
 def humanize(number):
     """Format a number to engineer scale"""
+    if is_list_like(number):
+        return', '.join(map(humanize, number))
     order = number and int(floor(log(abs(number)) / log(1000)))
     human_readable = ORDERS.split(" ")[int(order > 0)]
     if order == 0 or order > len(human_readable):
