5418dcdff8a511915e96776088a948b68e1a3b04
Florian Mounier <florian.mounier@kozea.fr>
2015-01-21 12:05:45 +0100

    Fix #176

--- pygal/config.py
+++ pygal/config.py
@@ -243,19 +243,19 @@ class Config(CommonConfig):
         "Margin around chart")
 
     margin_top = Key(
-        20, int, "Look",
+        None, int, "Look",
         "Margin around top of chart")
 
     margin_right = Key(
-        20, int, "Look",
+        None, int, "Look",
         "Margin around right of chart")
 
     margin_bottom = Key(
-        20, int, "Look",
+        None, int, "Look",
         "Margin around bottom of chart")
 
     margin_left = Key(
-        20, int, "Look",
+        None, int, "Look",
         "Margin around left of chart")
 
     tooltip_border_radius = Key(0, int, "Look", "Tooltip border radius")
