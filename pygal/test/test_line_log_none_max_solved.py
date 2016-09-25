4b3fc71664df6c4c3e3aabb108e10c8c58497706
qins <qinshuai@sjtu.edu.cn>
2016-09-25 16:38:09 +0800

    Solve #309 with flake8 constrain

--- pygal/test/test_line_log_none_max_solved.py
+++ pygal/test/test_line_log_none_max_solved.py
@@ -8,8 +8,8 @@ from pygal import Line
 from pygal.test.utils import texts
 from math import cos, sin
 
-chart = Line(title='test', x_label_rotation=90, human_readable=True, logarithmic=True)
-chart.add('test 1', [None, None, None, None, -38, 48, None, 4422, 34443, 345586, 40003452, 1235533, 2235533])
-chart.add('test 2', [ 1, 7, 19, 30, 40, 20, 38, 283, 2937, 29374, 20399, 293874, 3947])
+chart = Line(title='test', logarithmic=True)
+chart.add('test 1', [None, -38, 48, 4422, 35586, 1003452, 225533])
+chart.add('test 2', [1, 40, 20, 38, 2937, 20399, 3947])
 q = chart.render_pyquery()
 assert len(q(".dots")) == 20
