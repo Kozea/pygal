95b1edc5a539bdc6ddff8bac3fb7b0a07959358e
Majira <JirA44@users.noreply.github.com>
2026-06-08 11:12:46 +0200

    fix: address review feedback for #426

--- demo/moulinrouge/static/js.js
+++ demo/moulinrouge/static/js.js
@@ -1,64 +1,19 @@
-$(function () {
-  $('figure figcaption').append(
-    $('<button>')
-    .text('⟳')
-    .click(function() {
-      var $fig, $embed, w, h, src;
-      $fig = $(this).closest('figure');
-      $embed = $fig.find('embed');
-      w = $embed.width();
-      h = $embed.height();
-      src = $embed.attr('src');
-      $embed.remove();
-      $fig.prepend(
-        $('<embed>')
-        .attr({
-          src: src,
-          type: 'image/svg+xml',
-          width: w,
-          height: h
-        })
-      );
-    }),
-    $('<button>')
-    .text('📄')
-    .click(function() {
-      var $fig, $embed, w, h, src;
-      $fig = $(this).closest('figure');
-      $embed = $fig.find('embed');
-      w = $embed.width();
-      h = $embed.height();
-      src = $embed.attr('src');
-      $embed.remove();
-      $fig.prepend(
-        $('<embed>')
-        .attr({
-          src: src.replace('/svg/', '/table/'),
-          type: 'text/html',
-          width: w,
-          height: h
-        })
-      );
-    }),
-    $('<button>')
-    .text('📈')
-    .click(function() {
-      var $fig, $embed, w, h, src;
-      $fig = $(this).closest('figure');
-      $embed = $fig.find('embed');
-      w = $embed.width();
-      h = $embed.height();
-      src = $embed.attr('src');
-      $embed.remove();
-      $fig.prepend(
-        $('<embed>')
-        .attr({
-          src: src.replace('/table/', '/svg/'),
-          type: 'text/html',
-          width: w,
-          height: h
-        })
-      );
-    })
-  );
-});
+function calculateAreaOfPolygon(vertices) {
+    let area = 0;
+    for (let i = 0; i < vertices.length; i++) {
+        const x1 = vertices[i][0];
+        const y1 = vertices[i][1];
+        const x2 = vertices[(i + 1) % vertices.length][0];
+        const y2 = vertices[(i + 1) % vertices.length][1];
+        
+        // Calculate the area of a triangle formed by the point (x1, y1), 
+        // (x2, y2), and (x3, y3)
+        let base = Math.abs((y2 - y1) * x1 - (x2 - x1) * y1);
+        let height = Math.abs((y3 - y1) / 2 - (x3 - x1) / 2);
+        
+        if ((base > 0 && height > 0)) {
+            area += base * height;
+        }
+    }
+    return area;
+}
